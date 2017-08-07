9803cd7af9d415f90c874c5df8737a47f2b8f860
marhuh <markus.huh@example.com>
2017-08-07 17:34:37 +0300

    test

--- app/src/main/java/com/huhtamaki/marhuh/simplenotifications/CustomAdapter.java
+++ app/src/main/java/com/huhtamaki/marhuh/simplenotifications/CustomAdapter.java
@@ -1,8 +1,18 @@
 package com.huhtamaki.marhuh.simplenotifications;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
 /**
  * Created by marhuh on 7.8.2017.
  */
 
-public class CustomAdapter {
+class CustomAdapter extends ArrayAdapter<String>{
+
+
+    public CustomAdapter(@NonNull Context context, String[] networks) {
+        super(context,R.layout.custom_row,networks);
+    }
 }
