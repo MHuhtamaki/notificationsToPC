e633da1ae178b97c182724c91ec31c2941e3b8d8
marhuh <markus.huh@example.com>
2017-08-11 14:29:00 +0300

    commit 11.8 14:30 looking good

--- app/src/main/java/com/huhtamaki/marhuh/simplenotifications/CustomAdapter.java
+++ app/src/main/java/com/huhtamaki/marhuh/simplenotifications/CustomAdapter.java
@@ -3,6 +3,10 @@ package com.huhtamaki.marhuh.simplenotifications;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.media.Image;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -20,6 +24,8 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Objects;
 
+import static android.content.Context.CONNECTIVITY_SERVICE;
+
 /**
  * Created by marhuh on 7.8.2017.
  */
@@ -28,11 +34,18 @@ class CustomAdapter extends ArrayAdapter<String>{
 
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
+    Context context;
+    String connected_wifi_name;
 
     public CustomAdapter(@NonNull Context context, ArrayList<String> networks) {
         super(context,R.layout.custom_row,networks);
         prefs = context.getSharedPreferences("networkStates",context.MODE_PRIVATE);
         editor = prefs.edit();
+        this.context = context;
+
+        WifiManager wifiMgr = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifi_info = wifiMgr.getConnectionInfo();
+        connected_wifi_name = wifi_info.getSSID();
     }
 
     @NonNull
@@ -44,19 +57,26 @@ class CustomAdapter extends ArrayAdapter<String>{
         final String networkSSID = getItem(position);
         final TextView rowText = customView.findViewById(R.id.row_text);
         ImageView listImage = customView.findViewById(R.id.iw_wifi);
+        rowText.setText(networkSSID);
+
         Switch mySwitch = customView.findViewById(R.id.wifi_switch);
+        boolean value = prefs.getBoolean(networkSSID,false);
+        mySwitch.setChecked(value);
 
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                //rowText.setText(networkSSID + ", state:" + String.valueOf(b));
-                editor.putString(networkSSID,String.valueOf(b));
+            public void onCheckedChanged(CompoundButton compoundButton, boolean state) {
+                editor.putBoolean(networkSSID,state);
                 editor.apply();
             }
         });
 
-        rowText.setText(networkSSID);
-        listImage.setImageResource(R.drawable.wifi_icon_new);
+        if(rowText.getText().toString().equals(connected_wifi_name)){
+            listImage.setImageResource(R.drawable.wifi_icon_new);
+        }
+        else{
+            listImage.setImageResource(R.drawable.wifi_icon_new_unactive);
+        }
 
         return customView;
     }
--- app/src/main/java/com/huhtamaki/marhuh/simplenotifications/CustomAppAdapter.java
+++ app/src/main/java/com/huhtamaki/marhuh/simplenotifications/CustomAppAdapter.java
@@ -1,14 +1,18 @@
 package com.huhtamaki.marhuh.simplenotifications;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,8 +23,13 @@ import java.util.ArrayList;
 
 public class CustomAppAdapter extends ArrayAdapter<Application> {
 
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+
     public CustomAppAdapter(@NonNull Context context, ArrayList<Application> appList) {
         super(context,R.layout.app_custom_row ,appList);
+        prefs = context.getSharedPreferences("appStates",context.MODE_PRIVATE);
+        editor = prefs.edit();
     }
 
     @NonNull
@@ -30,13 +39,26 @@ public class CustomAppAdapter extends ArrayAdapter<Application> {
         View customView = inflater.inflate(R.layout.app_custom_row, parent, false);
 
         Application app = getItem(position);
-        String app_name = app.getName();
+        final String app_name = app.getName();
         Drawable app_icon = app.getIcon();
 
         TextView row_text = customView.findViewById(R.id.app_name);
         ImageView row_image = customView.findViewById(R.id.app_img);
         row_text.setText(app_name);
         row_image.setImageDrawable(app_icon);
+
+        Switch mySwitch = customView.findViewById(R.id.app_switch);
+        boolean value = prefs.getBoolean(app_name,false);
+        mySwitch.setChecked(value);
+
+
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean state) {
+                editor.putBoolean(app_name,state);
+                editor.apply();
+            }
+        });
 
         return customView;
 
